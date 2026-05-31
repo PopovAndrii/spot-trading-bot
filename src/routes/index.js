fcffe3c7fd7c7696033c7f0a7d0177d9666a5b45
Andrii Popov <autoxarkov@gmail.com>
2026-05-31 01:05:40 +0100

    Main P: add current ip adress info

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const { InvokeApi } = require('../lib/invokeAPI');
+const { getPublicIp } = require('../lib/serverIp');
 
 const apiMethod = new InvokeApi();
 
 router.get('/', async function (req, res, next) {
-  const result = await apiMethod.getSpotSymbols();
+  const [result, serverIp] = await Promise.all([
+    apiMethod.getSpotSymbols(),
+    getPublicIp(),
+  ]);
   res.render('index', {
     title: 'Main Page',
     info: result.success ? result.message : { symbols: [] },
+    serverIp,
   });
 });
 
